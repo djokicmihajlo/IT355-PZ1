44035dcd3b01517eb3459740638e9a645eaf69d0
Mihajlo <djokicsoffice@gmail.com>
2026-06-15 11:20:15 +0200

    Initial Spring MVC project setup

---
+++ src/main/java/com/it355pz/freelance/FreelancePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.it355pz.freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancePlatformApplication.class, args);
+	}
+
+}
